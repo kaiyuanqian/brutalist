1d02bf04d246a3e8d68e9bc409cb91c954ebe43f
Kaiyuan Qian <116858989+kaiyuanqian@users.noreply.github.com>
2023-05-07 17:54:36 +1000

    make triangles same size

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function spawnTriangle() {
     const triangles1 = document.querySelector('.triangles1');
     const triangles2 = document.querySelector('.triangles2');
 
-    let height = '7';
+    let height = '10';
 
     // create 19 triangles
-    for (let i = 0; i < 12; i++) {
+    for (let i = 0; i < 10; i++) {
         const triangle1 = document.createElement('div');
         const triangle2 = document.createElement('div');
         
@@ -15,9 +15,9 @@ function spawnTriangle() {
         triangle1.style.height = 0;
         triangle1.style.width = 0;
         triangle1.style.borderTop = '0px';
-        triangle1.style.borderRight = b + 'vmax solid transparent';
-        triangle1.style.borderBottom = height + 'vmax solid red';
-        triangle1.style.borderLeft = b + 'vmax solid transparent';
+        triangle1.style.borderRight = b + 'vh solid transparent';
+        triangle1.style.borderBottom = height + 'vh solid red';
+        triangle1.style.borderLeft = b + 'vh solid transparent';
 
         triangle1.style.transform = 'rotate(' + randNum(0, 360) + 'deg)';
 
@@ -25,15 +25,15 @@ function spawnTriangle() {
         triangle2.style.height = 0;
         triangle2.style.width = 0;
         triangle2.style.borderTop = '0px';
-        triangle2.style.borderRight = b + 'vmax solid transparent';
-        triangle2.style.borderBottom = height + 'vmax solid red';
-        triangle2.style.borderLeft = b + 'vmax solid transparent';
+        triangle2.style.borderRight = b + 'vh solid transparent';
+        triangle2.style.borderBottom = height + 'vh solid red';
+        triangle2.style.borderLeft = b + 'vh solid transparent';
 
         triangle2.style.transform = 'rotate(' + randNum(0, 360) + 'deg)';
         
         triangles1.appendChild(triangle1);
         triangles2.appendChild(triangle2);
-        height -= 0.5;
+        // height -= 0.5;
 
     }
 }
